4ecfc58d916744b10f4f86e2c1409ce85f383bfa
Bernd Heidemann <bernd.heidemann@googlemail.com>
2019-10-30 17:00:39 +0100

    typo in discount

--- src/discount/DiscountCalcTest.java
+++ src/discount/DiscountCalcTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class DiscountCalcTest {
 
     @Test
-    public void given_2000_When_Calc_Then_1800() {
+    public void given_2000_when_calc_Then_1800() {
         DiscountCalc discount=new DiscountCalc();
         assertEquals(1800, discount.calc(2000));
 
