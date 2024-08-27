3a29bad5c2d75b844beec09d896cdbc79dd8ce27
Bernd Heidemann <bernd.heidemann@gmail.com>
2024-08-27 12:35:33 +0200

    discount

--- src/discount/DiscountCalc.java
+++ src/discount/DiscountCalc.java
@@ -1,7 +1,12 @@
 package discount;
 
 public class DiscountCalc {
+    // amount in Cent
     public int calc(int amount) {
-        return 0;
+        if (amount>1000) {
+            return amount*90/100;  // 10% wird abgezogen
+        } else {
+            return amount;     // hier wird gar nichts abgezogen
+        }
     }
 }
